68fa38dcbd598776dbde0fa009bd8503e811cb79
RaniRahmaWulan <rani_194@smkassalaambandung.sch.id>
2022-07-26 15:26:48 +0700

    Test Project Gits

---
+++ soal1.php
@@ -0,0 +1,22 @@
+<?php
+
+    $noA = [1 , 1 , 0 , 1 , 1 , 1];
+
+    
+    echo "<b><i>Soal 1</i></b>" . "<br>";
+    echo "Jawaban;" . "<br>";
+    echo "$noA[0]" + "$noA[1]" * "$noA[2]" / "$noA[3]" + "$noA[4]" + "$noA[5]" . "<br>";
+   
+    
+    $noB = [1 , 0 , 0 , 1 , 0 , 1 , 1];
+
+    echo "Jawaban;" . "<br>";
+    echo "$noB[0]" + "$noB[1]" * "$noB[2]" / "$noB[3]" -  "$noB[4]" / "$noB[5]" + "$noB[6]" . "<br>";
+    echo "<br>";
+
+    $a = ["H" , "E" , "L" , "L" , "O"];
+
+    echo "<b><i>Soal 2</i></b>" . "<br>";
+    echo "Jawaban;" . "<br>";
+    echo "$a[4]" . "." . "$a[3]" . "." . "$a[2]" . "."  . "$a[1]" . "." . "$a[0]";
+?>
